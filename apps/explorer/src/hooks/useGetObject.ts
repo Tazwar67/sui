3a4e7a3b0cbfb9bfae6ced4f771c1688d730c60c
Godwin Jibs <g.jibuike@mystenlabs.com>
2023-04-10 11:41:55 -0400

    use core version

--- apps/explorer/src/hooks/useGetObject.ts
+++ apps/explorer/src/hooks/useGetObject.ts
@@ -2,35 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { useRpcClient } from '@mysten/core';
-import { type SuiObjectResponse, normalizeSuiAddress } from '@mysten/sui.js';
-import { useQuery, type UseQueryResult } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 
+//TODO: (Jibz) - In another PR, remove this once merged into core
 export function useGetSystemObject() {
     const rpc = useRpcClient();
     return useQuery(['system', 'state'], () => rpc.getLatestSuiSystemState());
-}
-
-export function useGetObject(
-    objectId?: string | null
-): UseQueryResult<SuiObjectResponse, unknown> {
-    const rpc = useRpcClient();
-    const normalizedObjId = objectId && normalizeSuiAddress(objectId);
-    const response = useQuery(
-        ['object', normalizedObjId],
-        async () =>
-            rpc.getObject({
-                id: normalizedObjId!,
-                options: {
-                    showType: true,
-                    showContent: true,
-                    showOwner: true,
-                    showPreviousTransaction: true,
-                    showStorageRebate: true,
-                    showDisplay: true,
-                },
-            }),
-        { enabled: !!normalizedObjId }
-    );
-
-    return response;
 }
